b3ec096a0244b96b22425a032fefdf905f1abcc6
abin jose <abin111manjapra@gmail.com>
2019-02-19 19:16:00 +0530

    purchase print fix

--- resources/views/purchases/estimate.blade.php
+++ resources/views/purchases/estimate.blade.php
@@ -1,15 +1,5 @@
 @extends('layouts.app')
 @section('title', 'Purchase Receipt')
-@section('stylesheets')
-<style>
-    @page{
-        margin-left: 0px;
-        margin-right: 0px;
-        margin-top: 10px;
-        margin-bottom: 10px;
-    }
-</style>
-@endsection
 @section('content')
 <div class="content-wrapper">
      <section class="content-header">
@@ -24,8 +14,9 @@
     </section>
     <!-- Main content -->
     <section class="invoice">
-        <div class="row no-print">
+        <div class="row">
             <div class="col-md-12">
+                @include('sections.print-head')
                 <h6 class="text-center">Receipt of purchase</h6>
                 <table class="table table-bordered" style="margin-bottom: 0px;">
                     <tbody>
@@ -189,141 +180,6 @@
                         </tr>
                     </tbody>
                 </table>
-            </div>
-        </div>
-        <div class="row visible-print-block">
-            <div class="col-md-12">
-                @include('sections.print-head')
-                <h6 class="text-center">Receipt of purchase</h6>
-                <i class="invoice-table-content">
-                    Ref No : #{{ ($purchase->id < 100 ? '0' : ''). $purchase->id }}
-                </i>
-                <i class="pull-right invoice-table-content-pull-right">
-                    Date: {{ $purchase->date->format('d-m-Y') }}
-                </i><br>
-                <i class="invoice-table-content">
-                    Supplier : {{ $purchase->transaction->creditAccount->account_name }}<br>
-                    <i class="invoice-table-content">{{ $purchase->supplier_name }}, {{ $purchase->supplier_phone }}</i>
-                </i>
-                <hr style="margin-top: 5px; margin-bottom: 5px;">
-                <h6 class="invoice-table-content">Purchase Details</h6>
-                <table class="table table-bordered invoice-table-content">
-                    <thead>
-                        <tr>
-                            <th style="width: 5%;">#</th>
-                            <th style="width: 25%;">Product</th>
-                            <th style="width: 8%;">Gross Qty</th>
-                            <th style="width: 8%;">Nos</th>
-                            <th style="width: 8%;">Unit Wastage</th>
-                            <th style="width: 8%;">Total Wastage</th>
-                            <th style="width: 13%;">Net Qty</th>
-                            <th style="width: 10%;">Rate</th>
-                            <th style="width: 15%;">Amount</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($purchase->products as $index => $product)
-                            <tr>
-                                <td>{{ $index + 1 }}</td>
-                                <td>{{ $product->name }}</td>
-                                <td>
-                                    {{ $product->purchaseDetail->gross_quantity ?: $product->purchaseDetail->net_quantity }}
-                                    {{ $product->uom_code }}
-                                </td>
-                                <td>{{ $product->purchaseDetail->product_number ?: '-' }}</td>
-                                <td>{{ $product->purchaseDetail->unit_wastage ?: '-' }}</td>
-                                <td>{{ $product->purchaseDetail->total_wastage ?: 0 }}</td>
-                                <td>{{ $product->purchaseDetail->net_quantity }} {{ $product->uom_code }}</td>
-                                <td>{{ $product->purchaseDetail->rate }}/-</td>
-                                <td>{{ ($product->purchaseDetail->net_quantity * $product->purchaseDetail->rate) }}</td>
-                            </tr>
-                        @endforeach
-                        <tr>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td><strong>Total Price</strong></td>
-                            <td></td>
-                            <td>{{ ($purchase->total_amount + $purchase->discount) }}</td>
-                        </tr>
-                        @if(!empty($purchase->discount) && $purchase->discount > 0)
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td><strong>Discount</strong></td>
-                                <td></td>
-                                <td>{{ $purchase->discount or 0}}</td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td><strong>Total Bill</strong></td>
-                                <td></td>
-                                <td>{{ $purchase->total_amount }}</td>
-                            </tr>
-                        @endif
-                    </tbody>
-                </table>
-                <h6 class="invoice-table-content">Payment Details</h6>
-                <table class="table table-bordered invoice-table-content">
-                    <tbody>
-                        <tr>
-                            <td style="width: 5%;">#</td>
-                            <td style="width: 65%;">Total Bill</td>
-                            <td style="width: 15%;">{{ $purchase->total_amount }}</td>
-                            <td style="width: 15%;">-</td>
-                        </tr>
-                        <tr>
-                            <td>#</td>
-                            @if($oldBalance == 0)
-                                <td>Old Balance</td>
-                                <td>-</td>
-                                <td>-</td>
-                            @elseif($oldBalance < 0)
-                                <td>
-                                    Old Balance 
-                                    <b> (Payable To {{ $purchase->transaction->creditAccount->account_name }})</b>
-                                </td>
-                                <td>{{ abs($oldBalance) }}</td>
-                                <td>-</td>
-                            @else
-                                <td>
-                                    Old Balance 
-                                    <b> (Recievable From {{ $purchase->transaction->creditAccount->account_name }})</b>
-                                </td>
-                                <td></td>
-                                <td>{{ abs($oldBalance) }}</td>
-                            @endif
-                        </tr>
-                        <tr>
-                            <td>#</td>
-                            <td>Cash Paid To Supplier</td>
-                            <td>-</td>
-                            <td>{{ (!empty($purchase->payment) && $purchase->payment->amount > 0) ? $purchase->payment->amount : '-'  }}</td>
-                        </tr>
-                    </tbody>
-                </table>
-                @if($oldBalance - $purchase->total_amount + (!empty($purchase->payment) ? $purchase->payment->amount : 0) == 0)
-                    <h5 class="invoice-table-content">Outstanding Balance = 0/-
-                    </h5>
-                @elseif($oldBalance - $purchase->total_amount + (!empty($purchase->payment) ? $purchase->payment->amount : 0) > 0)
-                    <h5 class="invoice-table-content">Outstanding Balance (Recievable From {{ $purchase->transaction->creditAccount->account_name }}) = {{ abs($oldBalance - $purchase->total_amount) + (!empty($purchase->payment) ? $purchase->payment->amount : 0) }}/-
-                    </h5>
-                @else
-                    <h5 class="invoice-table-content">Outstanding Balance (Payable To {{ $purchase->transaction->creditAccount->account_name }}) = {{ abs($oldBalance - $purchase->total_amount) + (!empty($purchase->payment) ? $purchase->payment->amount : 0) }}/-
-                    </h5>
-                @endif
             </div>
         </div>
         <div class="row no-print">
